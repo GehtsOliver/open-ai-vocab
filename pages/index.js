c0c402076bc9e25d4a5f16a6950852f27868401f
Olli <64855110+GehtsOliver@users.noreply.github.com>
2023-01-07 19:21:10 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,7 @@ export default function Home() {
   const [vocab, setVocab] = useState("");
 
   const getCompletion = async () => {
+    console.log("API KEY IN ENV VARIABLE:" + process.env.OPENAIAPI_KEY)
     console.log(configuration);
     const response = await openai.createCompletion({
       model: "text-davinci-001",
